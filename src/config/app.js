cf8e8b72ad0ff76c01f158082a65f02d1dcf15b4
= <=>
2020-04-15 15:52:21 -0300

    refactor: modified error handlers folder

--- src/config/app.js
+++ src/config/app.js
@@ -8,13 +8,15 @@ const app = express();
 
 const routes = require('../routes');
 
-const { globalErrorHandler } = require('../handlers/globalErrorHandler');
+const { globalErrorHandler } = require('../errorHandlers/globalErrorHandler');
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(requestLogger);
+
 app.use('/api/v1', routes);
+
 app.use(errorLogger);
 app.use(globalErrorHandler);
 
