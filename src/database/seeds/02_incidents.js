5a85e193fe4d1b64eaee104b81a7451317aa3d45
= <=>
2020-04-23 17:49:07 -0300

    feat: added seeds to database for environment development

---
+++ src/database/seeds/02_incidents.js
@@ -0,0 +1,128 @@
+exports.seed = function (knex) {
+  return knex('incidents')
+    .del()
+    .then(function () {
+      return knex('incidents').insert([
+        {
+          id: 1,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 2,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 3,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 4,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 5,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 6,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 7,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 8,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 9,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 10,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 11,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 12,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 13,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 14,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+        {
+          id: 15,
+          title: 'Arte de Doar',
+          description:
+            'A Apae de Catanduva atende 15 municípios da região e a campanha será uma maneira dos contribuÍntes conhecerem um pouco do trabalho que é desenvolvido pelos alunos. São atendidos mais de 350 alunos na Apae.',
+          value: 1000.15,
+          ong_id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+        },
+      ]);
+    });
+};
