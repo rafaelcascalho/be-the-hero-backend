8cedcd149e7f8a486961290635bc81f1dbed8b96
= <=>
2020-04-27 20:15:40 -0300

    refactor: extracted the seed objects from the seed function to improve readability

--- src/database/seeds/01_ongs.js
+++ src/database/seeds/01_ongs.js
@@ -1,32 +1,34 @@
+const ongs = [
+  {
+    id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
+    name: 'APAE',
+    email: 'contato@apae.com.br',
+    whatsapp: '',
+    city: 'Goiânia',
+    uf: 'GO',
+  },
+  {
+    id: 'c03cddee-ea5a-454d-8418-6681801a77fc',
+    name: 'ASSOCIAÇÃO ÁGAPE',
+    email: 'abraaolumi@gmail.com',
+    whatsapp: '',
+    city: 'Goiânia',
+    uf: 'GO',
+  },
+  {
+    id: '246fd999-ea54-44e5-863c-7449c99916c2',
+    name: 'Abrigo Nosso Lar',
+    email: 'cleusaluiza@yahoo.com.br',
+    whatsapp: '',
+    city: 'Goiânia',
+    uf: 'GO',
+  },
+];
+
 exports.seed = function (knex) {
   return knex('ongs')
     .del()
     .then(function () {
-      return knex('ongs').insert([
-        {
-          id: 'e11d5899-d6e0-4cc4-931b-24ba6a136da2',
-          name: 'APAE',
-          email: 'contato@apae.com.br',
-          whatsapp: '',
-          city: 'Goiânia',
-          uf: 'GO',
-        },
-        {
-          id: 'c03cddee-ea5a-454d-8418-6681801a77fc',
-          name: 'ASSOCIAÇÃO ÁGAPE',
-          email: 'abraaolumi@gmail.com',
-          whatsapp: '',
-          city: 'Goiânia',
-          uf: 'GO',
-        },
-        {
-          id: '246fd999-ea54-44e5-863c-7449c99916c2',
-          name: 'Abrigo Nosso Lar',
-          email: 'cleusaluiza@yahoo.com.br',
-          whatsapp: '',
-          city: 'Goiânia',
-          uf: 'GO',
-        },
-      ]);
+      return knex('ongs').insert(ongs);
     });
 };
