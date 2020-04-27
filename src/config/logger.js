002bbc3919ae74bd4b6bcab1f517b8c1f0137e7e
= <=>
2020-04-27 20:14:41 -0300

    refactor: removed duplicated config lines

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,42 +1,34 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
-const requestLogger = expressWinston.logger({
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: './logs/errors.log',
-      level: 'error',
-    }),
-    new winston.transports.File({ filename: './logs/requests.log' }),
-  ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.simple()
-  ),
-  meta: true,
-  msg: 'HTTP {{req.method}} {{req.url}}',
-  expressFormat: true,
-  colorize: true,
-});
-
-const errorLogger = expressWinston.errorLogger({
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: './logs/errors.log',
-      level: 'error',
-    }),
-    new winston.transports.File({ filename: './logs/requests.log' }),
-  ],
-  format: winston.format.combine(
-    winston.format.errors({ stack: true }),
-    winston.format.colorize(),
-    winston.format.simple()
-  ),
-});
+const transportsOptions = [
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: './logs/errors.log',
+    level: 'error',
+  }),
+  new winston.transports.File({ filename: './logs/requests.log' }),
+];
 
 module.exports = {
-  errorLogger,
-  requestLogger,
+  requestLogger: expressWinston.logger({
+    transports: transportsOptions,
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+    meta: true,
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    expressFormat: true,
+    colorize: true,
+  }),
+
+  errorLogger: expressWinston.errorLogger({
+    transports: transportsOptions,
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple(),
+      winston.format.errors({ stack: true })
+    ),
+  }),
 };
